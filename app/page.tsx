8d378221e29ab441e9f31a8be735c009b1d79b99
xCHLOMIx <gutabarwaa@gmail.com>
2026-01-15 18:32:29 +0200

    chore(init): Initial commit with dependencies

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+
+        <div className="flex flex-col items-center gap-2 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            We&apos;ll get to you later
+          </h1>
+          <p className="text-muted-foreground">
+            Welcome to the taxxk app.
+          </p>
+          <button>Yaay</button>
+        </div>
+    </div>
+  );
+}
