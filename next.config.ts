e951dc60418e667f137335b0adeafdc07043e990
xCHLOMIx <gutabarwaa@gmail.com>
2026-01-16 22:09:57 +0200

    config(next): Add backend url from .env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    BACKEND_URL: process.env.BACKEND_URL,
+  },
 };
 
 export default nextConfig;
